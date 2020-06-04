7b0ea02bd44e04b11e35a20e9508f38b52715d33
basti <bastien.collignon@outlook.fr>
2020-06-04 13:24:29 +0200

    modif menu

--- src/components/BarreNavigation.js
+++ src/components/BarreNavigation.js
@@ -1,8 +1,14 @@
 import React from "react";
-import {Nav, Navbar} from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import styled from "styled-components";
-import {HashRouter as Router, Link} from "react-router-dom";
+import { HashRouter as Router, Link } from "react-router-dom";
+import 'antd/dist/antd.css';
+import { Menu } from 'antd';
+import {
+    HomeOutlined, BookOutlined, DashboardOutlined, SettingOutlined, LoginOutlined} from '@ant-design/icons';
 
+const { SubMenu } = Menu;
+/*
 const Styles = styled.div`
     .navbar {
     background-color: black;
@@ -22,8 +28,11 @@ const Styles = styled.div`
         &:hover: red;
     }
     `;
+    */
 
 export const BarreNavigation = () => (
+
+    /*
     <Styles>
         <Navbar expand="lg">
             <Navbar.Brand><Router><Link to="/">UML Challenge</Link></Router></Navbar.Brand>
@@ -37,5 +46,58 @@ export const BarreNavigation = () => (
             </Navbar.Collapse>
         </Navbar>
     </Styles>
+    
+
+
+
+       <Menu.Item >
+            Accueil
+        </Menu.Item>
+    */
+
+
+
+    <Menu mode="horizontal">
+
+
+
+        <Menu.Item key="accueil" icon={<HomeOutlined />}>
+            <Router><Link to="/accueil">Accueil</Link></Router>
+        </Menu.Item>
+
+        <Menu.Item key="cours" icon={<BookOutlined />}>
+            <Router><Link to="/cours" >Cours</Link></Router>
+        </Menu.Item>
+
+        
+        <SubMenu icon={<SettingOutlined />} title="Navigation Three - Submenu">
+            <Menu.ItemGroup title="Item 1">
+                <Menu.Item key="setting:1">Option 1</Menu.Item>
+                <Menu.Item key="setting:2">Option 2</Menu.Item>
+            </Menu.ItemGroup>
+            <Menu.ItemGroup title="Item 2">
+                <Menu.Item key="setting:3">Option 3</Menu.Item>
+                <Menu.Item key="setting:4">Option 4</Menu.Item>
+            </Menu.ItemGroup>
+        </SubMenu>
+
+        <Menu.Item key="dashboard" icon={<DashboardOutlined />}>
+            <Router><Link to="/dashboard" >Dashboard</Link></Router>
+        </Menu.Item>
+
+
+
+        <Menu.Item key="alipay">
+            <a href="https://arche.univ-lorraine.fr/" target="_blank" rel="noopener noreferrer">
+                Accès à Arche
+          </a>
+        </Menu.Item>
+
+        <Menu.Item key="connexion" icon={<LoginOutlined />}>
+            <Router><Link to="./">Connexion</Link></Router>
+        </Menu.Item>
+
+        
+    </Menu>
 
 )
