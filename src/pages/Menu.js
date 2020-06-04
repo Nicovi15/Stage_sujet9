45f109e55848827bac461066623bc8d74d42de18
Valentin <Valentin@orange.fr>
2020-06-04 16:09:07 +0200

    test import theme

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,20 +1,35 @@
 import React, {Component} from 'react'
-
+import axios from 'axios';
 export default class Menu extends Component {
 
 
     constructor(props) {
         super(props);
-
-
+        this.state = {
+            theme:[],
+            difficulte:[1,2,3]
+        };
     }
+
+    componentDidMount() {
+      axios.get(`https://devweb.iutmetz.univ-lorraine.fr/~cazzoli2u/quizzuml/getTheme.php`)
+         .then(res => {
+           console.log('Salut');
+           console.log(res);
+           console.log(res.data);
+         })
+     }
 
 
     render() {
         return (
             <div>
                 <h1>C'est la page où se trouve les menus des questionnaires</h1>
-            </div>
+                <ul>
+                  {this.state.difficulte.map(difficulte => <li>{difficulte}</li>)}
+                </ul>
+
+          </div>
         );
     }
 }
