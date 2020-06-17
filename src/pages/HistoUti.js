488cda99736d8a1c98f9f0663f94154ea4c175d2
Nicovi15 <nclsvivier15@gmail.com>
2020-06-17 11:44:29 +0200

    hisot

--- src/pages/HistoUti.js
+++ src/pages/HistoUti.js
@@ -3,9 +3,26 @@ import {HashRouter as Router, Link, Redirect} from "react-router-dom";
 
 export default class HistoUti extends Component {
 
+
+    constructor() {
+        super();
+        this.state = {
+            id:null,
+        }
+    }
+
+
+
+    componentDidMount() {
+        this.setState({
+            id : this.props.match.params.id,
+        });
+    }
+
 render(){
     return(
-            <h1>Je suis la page d'historique utilisateur</h1>
+            <h1>Je suis la page d'historique utilisateur + {this.state.id}</h1>
+
     )
 }
 
